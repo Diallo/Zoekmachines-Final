a047e1e550d7054e0f073f7410d77d9620c7a476
Mo Diallo <mnl.diallo@gmail.com>
2018-10-18 12:19:39 +0200

    Added dataset (Do not do this in practice ever)

--- install_scripts/collect_dataset.py
+++ install_scripts/collect_dataset.py
@@ -1,3 +1,8 @@
 """
 Collect the dataset and place it within the data folder.
 """
+import wget
+
+url = "http://zoekmachinesdata.mdiallo.nl/datasets/ted_talks/ted-talks.zip"
+
+file_name = wget.download(url)
--- install_scripts/format_dataset.py
+++ install_scripts/format_dataset.py
@@ -1,3 +1,43 @@
 """
 Format the dataset and place it within the data folder.
 """
+
+import csv
+import json
+
+all_data = []
+
+with open('../data/ted_main.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    header_title = None
+    for row in csv_reader:
+        if line_count == 0:
+            header_title = row.copy()
+            line_count += 1
+        else:
+            temp_dict = {}
+            for i,col in enumerate(row):
+                temp_dict[header_title[i]] = col
+            all_data.append(temp_dict.copy())
+            line_count+=1
+
+
+with open('../data/transcripts.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+
+            for talk_dict in all_data:
+                if talk_dict['url'] == row[1]:
+                    talk_dict['transcript'] = row[0]
+                    break
+            line_count+=1
+
+
+
+f = open("talks_data.json", "w")
+f.write(json.dumps(all_data))
