54f780807027a213c0c6b6737eb2304db108a2ea
robin <robinwacco11@hotmail.com>
2018-10-18 09:30:50 +0000

    dingen

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+    
+    
+@app.route('/search')
+def search():
+    return render_template('search.html')
+    
+    
+@app.route('/res')
+def result():
+    return render_template('result.html')
 
 
-app.run()
+app.run(debug=True, host='0.0.0.0')
